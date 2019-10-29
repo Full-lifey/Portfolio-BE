c2c70f7393e585c2149e434111fa1d7574992100
Joel Perez <joelness03@gmail.com>
2019-10-29 15:46:33 -0600

    edited description field length

--- data/migrations/20190906084847_create_projects_table.js
+++ data/migrations/20190906084847_create_projects_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("projects", tbl => {
     tbl.increments();
     tbl.string("title", 128).notNullable();
-    tbl.string("description", 512).notNullable();
+    tbl.string("description", 1024).notNullable();
     tbl.string("imageURL").notNullable();
     tbl.string("deployment", 512).notNullable();
     tbl.string("repo", 512).notNullable();
