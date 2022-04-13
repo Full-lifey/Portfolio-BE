0a90239e1d4385768b8967f597379aeef772eddd
Joel Perez <joel.perez@rocketcom.com>
2022-04-13 17:02:33 -0400

    add project findById route

--- project/projects-router.js
+++ project/projects-router.js
@@ -1,51 +1,61 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const Projects = require("./projects-model.js");
+const Projects = require('./projects-model.js');
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
+  console.log(Projects.find());
   Projects.find()
-    .then(projects => {
+    .then((projects) => {
       res.status(200).json(projects);
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ message: "Server error: unable to retrieve projects" });
+    .catch((err) => {
+      res.status(500).json({ message: `Server error: ${err}` });
     });
 });
 
-router.post("/", (req, res) => {
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Projects.findById(id)
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Server error: ${err}` });
+    });
+});
+
+router.post('/', (req, res) => {
   const project = req.body;
   Projects.add(project)
-    .then(createdProject => {
+    .then((createdProject) => {
       res.status(200).json(createdProject);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
 
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
   const { id } = req.params;
   Projects.update(id, req.body)
-    .then(updatedProject => {
+    .then((updatedProject) => {
       res.status(200).json(updatedProject);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   const { id } = req.params;
   Projects.remove(id)
-    .then(deleted => {
-      res.status(204).json({ message: "Successfully deleted project" });
+    .then((deleted) => {
+      res.status(204).json({ message: 'Successfully deleted project' });
     })
-    .catch(err => {
+    .catch((err) => {
       res
         .status(500)
-        .json({ message: "Server error: The project was not deleted" });
+        .json({ message: 'Server error: The project was not deleted' });
     });
 });
 module.exports = router;
